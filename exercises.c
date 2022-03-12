a1acf80efe2c6a99990da74fc2c07b9ec983b6e5
antonia navarrete silva <antonia.navarrete.s@mail.pucv.cl>
2022-03-12 00:32:37 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -39,9 +39,14 @@ Utilice la función sumaN.
 
 void sumaNultimos(int a[], int n, int m, int * suma) {
 
-   
-   
    *suma = sumaN( a, n);
+
+   for (int i = 0 ; i < n ; i++){
+      if ( i < n-m){
+         *suma = *suma - a[i]
+      }
+
+   }
    
 }
 
