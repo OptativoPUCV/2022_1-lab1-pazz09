aaecd6acfde482aed0cbcb1fb6c6b764980a93b3
antonia navarrete silva <antonia.navarrete.s@mail.pucv.cl>
2022-03-11 23:39:06 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -43,7 +43,7 @@ void sumaNultimos(int a[], int n, int m, int * suma) {
 
 
    for (int i = 0; i < n; i++){
-      if(i+1 > n-m){
+      if(10 > n-m){
          
          *suma = sumaN( a, n);
       }
